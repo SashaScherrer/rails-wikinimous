d245532448e9925158d4329e94df1c01b0c152b2
Sasha Scherrer <sasha.burkhardt@hotmail.fr>
2023-05-23 16:06:13 +0200

    Wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,15 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+require 'faker'
+
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: 3).join("\n\n")
+  )
+end
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
